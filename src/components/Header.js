c5c7bb1f45083a14c59c21a9accf4f796557f199
Ryan Elainska <ryan@elainska.com>
2019-01-17 13:15:49 -0500

    Change site title, experiment with now configs, exclude .netlify/

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,12 @@ import PropTypes from 'prop-types';
 const Header = props => (
   <header className="top">
     <h1>
-      Catch
+      Filet
       <span className="ofThe">
-        <span className="of">Of</span>
-        <span className="the">The</span>
+        <span className="of">&nbsp;</span>
+        <span className="the">&nbsp;</span>
       </span>
-      Day
+      That
     </h1>
     <h3 className="tagline">
       <span>{props.tagline}</span>
